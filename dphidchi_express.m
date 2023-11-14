23cf7422843ab13f91d414fac39b3dd1ab8f2b16
zi-tieguang <150761618+zi-tieguang@users.noreply.github.com>
2023-11-14 10:50:11 +0800

    Add files via upload

---
+++ dphidchi_express.m
@@ -0,0 +1,21 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+(e*p^2*(-4*M*x - 4*e*M*x*Cos[\[Chi]] + p*(a*En + x)*
+    (1 + Sqrt[(p^3 - 8*M*\[Alpha] - 4*e*M*\[Alpha]*Cos[\[Chi]]*
+         (6 + e^2 + 6*e*Cos[\[Chi]] + e^2*Cos[2*\[Chi]]))/p^3]))*Sin[\[Chi]])/
+ ((1 + e*Cos[\[Chi]])^2*(p^2 - 4*M*p*(1 + e*Cos[\[Chi]]) + 
+   (a + a*e*Cos[\[Chi]])^2 + 
+   a^2*Sqrt[1 - (8*M*\[Alpha]*(1 + e*Cos[\[Chi]])^3)/p^3] + 
+   p^2*Sqrt[1 - (8*M*\[Alpha]*(1 + e*Cos[\[Chi]])^3)/p^3] + 
+   2*a^2*e*Cos[\[Chi]]*Sqrt[1 - (8*M*\[Alpha]*(1 + e*Cos[\[Chi]])^3)/p^3] + 
+   a^2*e^2*Cos[\[Chi]]^2*Sqrt[1 - (8*M*\[Alpha]*(1 + e*Cos[\[Chi]])^3)/p^3])*
+  Sqrt[(p*(-8*a*En*M*(a*En + x)*(1 + e*Cos[\[Chi]])^2 - 
+      (a*En + x)^2*(1 + e*Cos[\[Chi]])*(p - 4*M*(1 + e*Cos[\[Chi]]) + 
+        p*Sqrt[1 - (8*M*\[Alpha]*(1 + e*Cos[\[Chi]])^3)/p^3]) + 
+      p^2*(4*M + ((-1 + En^2)*p*(1 + Sqrt[1 - (8*M*\[Alpha]*(1 + 
+                 e*Cos[\[Chi]])^3)/p^3]))/(1 + e*Cos[\[Chi]])) - 
+      a^2*(1 + e*Cos[\[Chi]])*
+       (p*(1 + Sqrt[1 - (8*M*\[Alpha]*(1 + e*Cos[\[Chi]])^3)/p^3]) - 
+        En^2*(4*M + p + 4*e*M*Cos[\[Chi]] + 
+          p*Sqrt[1 - (8*M*\[Alpha]*(1 + e*Cos[\[Chi]])^3)/p^3]))))/
+    ((1 + e*Cos[\[Chi]])^3*(1 + Sqrt[1 - (8*M*\[Alpha]*(1 + e*Cos[\[Chi]])^3)/
+         p^3]))])
---
+++ dtdchi_express.m
@@ -0,0 +1,25 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+(e*p^2*(En*p^3 - 4*a*M*x*(1 + e*Cos[\[Chi]])^3 + 
+   En*p*(a + a*e*Cos[\[Chi]])^2 + 
+   a^2*En*p*Sqrt[1 - (8*M*\[Alpha]*(1 + e*Cos[\[Chi]])^3)/p^3] + 
+   En*p^3*Sqrt[1 - (8*M*\[Alpha]*(1 + e*Cos[\[Chi]])^3)/p^3] + 
+   2*a^2*e*En*p*Cos[\[Chi]]*Sqrt[1 - (8*M*\[Alpha]*(1 + e*Cos[\[Chi]])^3)/
+       p^3] + a^2*e^2*En*p*Cos[\[Chi]]^2*
+    Sqrt[1 - (8*M*\[Alpha]*(1 + e*Cos[\[Chi]])^3)/p^3])*Sin[\[Chi]])/
+ ((1 + e*Cos[\[Chi]])^4*(p^2 - 4*M*p*(1 + e*Cos[\[Chi]]) + 
+   (a + a*e*Cos[\[Chi]])^2 + 
+   a^2*Sqrt[1 - (8*M*\[Alpha]*(1 + e*Cos[\[Chi]])^3)/p^3] + 
+   p^2*Sqrt[1 - (8*M*\[Alpha]*(1 + e*Cos[\[Chi]])^3)/p^3] + 
+   2*a^2*e*Cos[\[Chi]]*Sqrt[1 - (8*M*\[Alpha]*(1 + e*Cos[\[Chi]])^3)/p^3] + 
+   a^2*e^2*Cos[\[Chi]]^2*Sqrt[1 - (8*M*\[Alpha]*(1 + e*Cos[\[Chi]])^3)/p^3])*
+  Sqrt[(p*(-8*a*En*M*(a*En + x)*(1 + e*Cos[\[Chi]])^2 - 
+      (a*En + x)^2*(1 + e*Cos[\[Chi]])*(p - 4*M*(1 + e*Cos[\[Chi]]) + 
+        p*Sqrt[1 - (8*M*\[Alpha]*(1 + e*Cos[\[Chi]])^3)/p^3]) + 
+      p^2*(4*M + ((-1 + En^2)*p*(1 + Sqrt[1 - (8*M*\[Alpha]*(1 + 
+                 e*Cos[\[Chi]])^3)/p^3]))/(1 + e*Cos[\[Chi]])) - 
+      a^2*(1 + e*Cos[\[Chi]])*
+       (p*(1 + Sqrt[1 - (8*M*\[Alpha]*(1 + e*Cos[\[Chi]])^3)/p^3]) - 
+        En^2*(4*M + p + 4*e*M*Cos[\[Chi]] + 
+          p*Sqrt[1 - (8*M*\[Alpha]*(1 + e*Cos[\[Chi]])^3)/p^3]))))/
+    ((1 + e*Cos[\[Chi]])^3*(1 + Sqrt[1 - (8*M*\[Alpha]*(1 + e*Cos[\[Chi]])^3)/
+         p^3]))])
